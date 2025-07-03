ae1c3629b9c256f57970e53be01fc36e1ec9529b
nav2305 <it23226364@my.sliit.lk>
2025-07-03 21:50:32 +0530

    Initialized the question microservice

---
+++ questionService/src/test/java/com/nav/questionService/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nav.questionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
