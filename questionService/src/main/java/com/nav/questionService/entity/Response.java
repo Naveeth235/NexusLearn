ddc8daf09c100b59ee80c13bfe0e4a94937d7c20
nav2305 <it23226364@my.sliit.lk>
2025-07-03 22:03:24 +0530

    Added other required classes

---
+++ questionService/src/main/java/com/nav/questionService/entity/Response.java
@@ -0,0 +1,12 @@
+package com.nav.questionService.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+    private Integer id;
+    private String response;
+}
