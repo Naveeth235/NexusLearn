1546b34d7984dac41b36de105658d1b05137e47a
nav2305 <it23226364@my.sliit.lk>
2025-07-04 23:57:37 +0530

    Added the required entities for the quiz MS

---
+++ quizService/src/main/java/com/nav/quizService/entity/Quiz.java
@@ -0,0 +1,27 @@
+package com.nav.quizService.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    @ManyToMany
+    private List<Question> questions;
+}
+
---
+++ quizService/src/main/java/com/nav/quizService/entity/Response.java
@@ -0,0 +1,15 @@
+package com.nav.quizService.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+    private Integer id;
+    private String response;
+}
